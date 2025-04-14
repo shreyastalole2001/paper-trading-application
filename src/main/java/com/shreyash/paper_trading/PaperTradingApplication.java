2463fddff6fb22874e02e5cf518055443bc7ee10
Shreyash Talole <shreyastalole2001@gmail.com>
2025-04-14 10:09:03 +0530

    Initial Commit

---
+++ src/main/java/com/shreyash/paper_trading/PaperTradingApplication.java
@@ -0,0 +1,13 @@
+package com.shreyash.paper_trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperTradingApplication.class, args);
+	}
+
+}
